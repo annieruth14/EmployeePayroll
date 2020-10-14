4353aa4a6a3e272a4e136e1fc44fa3ca4f1170cc
annieruth14 <ruthannie3@gmail.com>
2020-10-14 12:42:30 +0530

    [Annie]Added: Welcome message

---
+++ src/main/java/com/bridgelabz/test/EmployeeIO/EmployeePayrollService.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.test.EmployeeIO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+	}
+}
